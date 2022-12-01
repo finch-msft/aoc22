7917976d628b4c44bf809049e567a81701e174aa
finch-msft <spencerfinch@microsoft.com>
2022-12-01 15:55:24 -0800

    1.2 done

--- day1.ps1
+++ day1.ps1
@@ -2,18 +2,23 @@ $input_elfcal = (Get-Content -path "./input/day1.txt" -Raw) -split "\r\n\r\n" # 
 
 $elf_dict = @{}  # remember: DOS newline encoding is \r\n, unix is \n
 $elves = 0 # basic iterator
-$max = (-1, -1) # record the biggest elf we've seen. Format: (elf, calories held)
 foreach ($elf in $input_elfcal){
     $elf_dict["Elf$elves"] = & { # is a dictionary the best choice? who cares. anyway this makes a new key with the return value of the script block
         $total_cals = 0
         foreach ($food_cals in $elf -Split("\r\n")){ # sums the calories of all foods the elf is carrying
             $total_cals += [int]$food_cals # casting probably overkill but you know implicit typed languages
         }
-        if ($total_cals -gt [int]$max[1]){ # if the calories on this elf are bigger than the previous biggest elf:
-            $script:max = ($elves, $total_cals) # assign new biggest elf and its calorie count. Script keyword needed because scoping just makes $max a new local var otherwise
-        }
-        $total_cals # return value
+        $total_cals
     }
     $elves++
 }
-Write-Host "Elf $($max[0]) has the most calories at $($max[1]), go take food from them :^)"
+Write-Host "______`n`nPART ONE: Top elf:" 
+$elf_dict.GetEnumerator() | Sort-Object Value | Select-Object -Last 1 # sort them by ascending value, get last entry (highest entry)
+Write-Host "______`n`nPART TWO: Top 3 elves carry this many calories total: " -NoNewline
+& {
+    $total # initalize variable to NULL, which == 0 in powershell
+    foreach ($calorie_count in $($elf_dict.GetEnumerator() | Sort-Object Value | Select-Object -Last 3).Value) { # sort them by ascending value, get only the values of the last 3 entries
+        $total += $calorie_count
+    }
+    $total
+} 
